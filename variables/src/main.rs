f67c8916c2df93ef9f10b70e5f4c35a4cce0b43e
Chuangshi Jiang <18251984729@163.com>
2022-05-10 11:48:25 +0800

    rust lesson 2.1

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,10 @@
+struct Struct {
+    e: i32
+}
 fn main() {
-    println!("Hello, world!");
+    let (a,b,c,d,e);
+    (a,b) = (1,2);
+    [c,..,d,_] = [1,2,3,4,5];
+    Struct {e,..} = Struct {e:5};
+    println!("is equal {:#?}",assert_eq!([1,2,1,4,5],[a,b,c,d,e]));
 }
