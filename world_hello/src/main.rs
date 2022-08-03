da88b6b31ac19fc1f76ed4bbacf4e960dd45a84f
ansai <jcsweiyi@outlook.com>
2022-08-04 07:20:46 +0800

    restart rust

--- world_hello/src/main.rs
+++ world_hello/src/main.rs
@@ -1,20 +1,6 @@
-fn add(i:i32,j:i32) -> i32 {
-    i+j
-}
-fn demo() {
-    let a = 10;
-    let b: i32 = 20;
-    let mut c = 30i32;
-    c += 40_i32;
-    let d = 30_i32;
-    let e = add(add(a, b),add(c, d));
-    println!("(a+b) + (c+d)={}",e)
-}
-
 fn main() {
-    demo();
     let penguin_data = "\
-    common name length (cm)
+    common name,length (c&m)
     Little penguin,33
     Yellow-eyed penguin,65
     Fiordland penguin,60
@@ -25,18 +11,13 @@ fn main() {
         if i == 0 || record.trim().len() == 0 {
             continue;
         }
-
-        let fields: Vec<_> = record
-        .split(',')
-        .map(|field| field.trim())
-        .collect();
-
+        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
         if cfg!(debug_assertions) {
-            eprintln!("debug: {:?} -> {:?}",record,fields);
+            eprintln!("debug:{:?}->{:?}", record, fields);
         }
         let name = fields[0];
         if let Ok(length) = fields[1].parse::<f32>() {
-            println!("{}, {}cm", name, length);
+            println!("{},{}cm", name, length)
         }
     }
 }
