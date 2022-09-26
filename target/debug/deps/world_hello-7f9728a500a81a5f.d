686838302824b3b6fdf900e53313a6deb859d4f3
ansai <jcsweiyi@outlook.com>
2022-09-26 22:54:52 +0800

    remove target folder

--- target/debug/deps/world_hello-7f9728a500a81a5f.d
+++
@@ -1,5 +0,0 @@
-/Users/jcs/Documents/workspace/draft-rust/world_hello/target/debug/deps/world_hello-7f9728a500a81a5f.rmeta: src/main.rs
-
-/Users/jcs/Documents/workspace/draft-rust/world_hello/target/debug/deps/world_hello-7f9728a500a81a5f.d: src/main.rs
-
-src/main.rs:
--- target/debug/world_hello.d
+++
@@ -1,1 +0,0 @@
-/home/jcs/workspace/draft-rust/target/debug/world_hello: /home/jcs/workspace/draft-rust/src/main.rs
