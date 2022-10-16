e18820b4bcb590135e7b7385c20410f229215089
root <root@meehotall-PC.localdomain>
2022-10-16 21:57:28 +0800

    base type

---
+++ base_type/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let _guess: i32 = "42".parse().expect("Not a number!");
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+
+    let addtion = twenty + twenty_one + twenty_two;
+    println!("{}+{}+{}={}", twenty, twenty_one, twenty_two, addtion);
+
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+
+    let forty_twos = [42.0, 42f32, 42.0_f32];
+    println!("{:.2}", forty_twos[0]);
+
+    for i in 1..=5 {
+        println!("{}", i);
+    }
+
+    for i in 'a'..='z' {
+        println!("{}", i);
+    }
+}
